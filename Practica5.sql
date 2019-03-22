21ea3eea51ee3f76181c2d2e1dcb2e264b7be21f
Ju4nDani3l <42685734+Ju4nDani3l@users.noreply.github.com>
2019-03-22 15:46:06 -0600

    Add files via upload

---
+++ Practica5.sql
@@ -0,0 +1,380 @@
+INSERT INTO Alumnos (Id,Nombre,ApellidoPaterno,ApellidoMaterno,Matricula)
+Values(NEWID(),'Juan','Reyna','Garza',1937862);
+
+INSERT INTO Alumnos (Id,Nombre,ApellidoPaterno,ApellidoMaterno,Matricula)
+Values(NEWID(),'Pepe','Pica','Papas',151515);
+
+INSERT INTO Alumnos (Id,Nombre,ApellidoPaterno,ApellidoMaterno,Matricula)
+Values(NEWID(),'Roberto','Antonio','lala',1616584);
+
+INSERT INTO Alumnos (Id,Nombre,ApellidoPaterno,ApellidoMaterno,Matricula)
+Values(NEWID(),'Beto','Tamayo','cortez',787458);
+
+INSERT INTO Alumnos (Id,Nombre,ApellidoPaterno,ApellidoMaterno,Matricula)
+Values(NEWID(),'santacruz','castillo','piedra',5548877);
+
+INSERT INTO Alumnos (Id,Nombre,ApellidoPaterno,ApellidoMaterno,Matricula)
+Values(NEWID(),'amy','sad','garza',323265);
+
+INSERT INTO Alumnos (Id,Nombre,ApellidoPaterno,ApellidoMaterno,Matricula)
+Values(NEWID(),'Alex','montemayor','perez',1548545);
+
+INSERT INTO Alumnos (Id,Nombre,ApellidoPaterno,ApellidoMaterno,Matricula)
+Values(NEWID(),'Johnathan','zertuche','garza',166554);
+
+INSERT INTO Alumnos (Id,Nombre,ApellidoPaterno,ApellidoMaterno,Matricula)
+Values(NEWID(),'Sarah','urbina','montalvo',1488554);
+
+INSERT INTO Alumnos (Id,Nombre,ApellidoPaterno,ApellidoMaterno,Matricula)
+Values(NEWID(),'christian','bob','trump',1665486);
+
+
+INSERT INTO Edificios(Id,UbicacionDelEdificio)
+Values(NEWID(),'GarzaSada');
+
+INSERT INTO Edificios(Id,UbicacionDelEdificio)
+Values(NEWID(),'LazaroCardenas');
+
+INSERT INTO Edificios(Id,UbicacionDelEdificio)
+Values(NEWID(),'PinoZuares');
+
+INSERT INTO Edificios(Id,UbicacionDelEdificio)
+Values(NEWID(),'Barragan');
+
+INSERT INTO Edificios(Id,UbicacionDelEdificio)
+Values(NEWID(),'AlfonsoReyes');
+
+INSERT INTO Edificios(Id,UbicacionDelEdificio)
+Values(NEWID(),'PedroDeAlba');
+
+INSERT INTO Edificios(Id,UbicacionDelEdificio)
+Values(NEWID(),'Universidad');
+
+INSERT INTO Edificios(Id,UbicacionDelEdificio)
+Values(NEWID(),'Consitucion');
+
+INSERT INTO Edificios(Id,UbicacionDelEdificio)
+Values(NEWID(),'cuahtemoc');
+
+INSERT INTO Edificios(Id,UbicacionDelEdificio)
+Values(NEWID(),'MoronesPrieto');
+
+
+INSERT INTO Excursion(Nombre,Fecha,PrecioPorAlumno,Hora)
+Values(NEWID(),GETDATE(),5, GETDATE() );
+
+INSERT INTO Excursion(Nombre,Fecha,PrecioPorAlumno,Hora)
+Values(NEWID(),GETDATE(),10, GETDATE() );
+
+INSERT INTO Excursion(Nombre,Fecha,PrecioPorAlumno,Hora)
+Values(NEWID(),GETDATE(),2, GETDATE() );
+
+INSERT INTO Excursion(Nombre,Fecha,PrecioPorAlumno,Hora)
+Values(NEWID(),GETDATE(),3, GETDATE() );
+
+INSERT INTO Excursion(Nombre,Fecha,PrecioPorAlumno,Hora)
+Values(NEWID(),GETDATE(),23, GETDATE() );
+
+INSERT INTO Excursion(Nombre,Fecha,PrecioPorAlumno,Hora)
+Values(NEWID(),GETDATE(),32, GETDATE() );
+
+INSERT INTO Excursion(Nombre,Fecha,PrecioPorAlumno,Hora)
+Values(NEWID(),GETDATE(),45, GETDATE() );
+
+INSERT INTO Excursion(Nombre,Fecha,PrecioPorAlumno,Hora)
+Values(NEWID(),GETDATE(),05, GETDATE() );
+
+INSERT INTO Excursion(Nombre,Fecha,PrecioPorAlumno,Hora)
+Values(NEWID(),GETDATE(),87, GETDATE() );
+
+INSERT INTO Excursion(Nombre,Fecha,PrecioPorAlumno,Hora)
+Values(NEWID(),GETDATE(),14, GETDATE() );
+
+
+
+INSERT INTO Libros(Codigo,Edicion,FechaDePublicacion)
+Values(NEWID(),1,GETDATE());
+
+INSERT INTO Libros(Codigo,Edicion,FechaDePublicacion)
+Values(NEWID(),3,GETDATE());
+
+INSERT INTO Libros(Codigo,Edicion,FechaDePublicacion)
+Values(NEWID(),6,GETDATE());
+
+INSERT INTO Libros(Codigo,Edicion,FechaDePublicacion)
+Values(NEWID(),7,GETDATE());
+
+INSERT INTO Libros(Codigo,Edicion,FechaDePublicacion)
+Values(NEWID(),15,GETDATE());
+
+INSERT INTO Libros(Codigo,Edicion,FechaDePublicacion)
+Values(NEWID(),19,GETDATE());
+
+INSERT INTO Libros(Codigo,Edicion,FechaDePublicacion)
+Values(NEWID(),30,GETDATE());
+
+INSERT INTO Libros(Codigo,Edicion,FechaDePublicacion)
+Values(NEWID(),17,GETDATE());
+
+INSERT INTO Libros(Codigo,Edicion,FechaDePublicacion)
+Values(NEWID(),25,GETDATE());
+
+INSERT INTO Libros(Codigo,Edicion,FechaDePublicacion)
+Values(NEWID(),78,GETDATE());
+
+
+
+INSERT INTO Maestros(Id,Nombre,ApellidoPaterno,ApellidoMaterno,FechaDeNacimiento)
+Values(NEWID(),'Francisco','perez','Santiago',GETDATE());
+
+INSERT INTO Maestros(Id,Nombre,ApellidoPaterno,ApellidoMaterno,FechaDeNacimiento)
+Values(NEWID(),'julia','marquez','marquez',GETDATE());
+
+INSERT INTO Maestros(Id,Nombre,ApellidoPaterno,ApellidoMaterno,FechaDeNacimiento)
+Values(NEWID(),'liliana','gonzales','futterkiste',GETDATE());
+
+INSERT INTO Maestros(Id,Nombre,ApellidoPaterno,ApellidoMaterno,FechaDeNacimiento)
+Values(NEWID(),'isabella','aiona','abbott',GETDATE());
+
+INSERT INTO Maestros(Id,Nombre,ApellidoPaterno,ApellidoMaterno,FechaDeNacimiento)
+Values(NEWID(),'jao ','albeson','ackerman',GETDATE());
+
+INSERT INTO Maestros(Id,Nombre,ApellidoPaterno,ApellidoMaterno,FechaDeNacimiento)
+Values(NEWID(),'jamesD','ackerman','ames',GETDATE());
+
+INSERT INTO Maestros(Id,Nombre,ApellidoPaterno,ApellidoMaterno,FechaDeNacimiento)
+Values(NEWID(),'frederick','case','lee',GETDATE());
+
+INSERT INTO Maestros(Id,Nombre,ApellidoPaterno,ApellidoMaterno,FechaDeNacimiento)
+Values(NEWID(),'zonia','barber','baiey',GETDATE());
+
+INSERT INTO Maestros(Id,Nombre,ApellidoPaterno,ApellidoMaterno,FechaDeNacimiento)
+Values(NEWID(),'willian','hardy','ettinger',GETDATE());
+
+INSERT INTO Maestros(Id,Nombre,ApellidoPaterno,ApellidoMaterno,FechaDeNacimiento)
+Values(NEWID(),'alfred','jay','chester',GETDATE());
+
+
+
+
+INSERT INTO Materias(Id,NombreDeLaMateria,GradoDeLaMateria)
+Values(NEWID(),'Matematicas','4');
+
+INSERT INTO Materias(Id,NombreDeLaMateria,GradoDeLaMateria)
+Values(NEWID(),'Fisica','5');
+
+INSERT INTO Materias(Id,NombreDeLaMateria,GradoDeLaMateria)
+Values(NEWID(),'Hisotoria','2');
+
+INSERT INTO Materias(Id,NombreDeLaMateria,GradoDeLaMateria)
+Values(NEWID(),'Ciencias Naturales','1');
+
+INSERT INTO Materias(Id,NombreDeLaMateria,GradoDeLaMateria)
+Values(NEWID(),'Sistemas Operativos','3');
+
+INSERT INTO Materias(Id,NombreDeLaMateria,GradoDeLaMateria)
+Values(NEWID(),'programacion','8');
+
+INSERT INTO Materias(Id,NombreDeLaMateria,GradoDeLaMateria)
+Values(NEWID(),'estadistica','7');
+
+INSERT INTO Materias(Id,NombreDeLaMateria,GradoDeLaMateria)
+Values(NEWID(),'EducacionFisica','9');
+
+INSERT INTO Materias(Id,NombreDeLaMateria,GradoDeLaMateria)
+Values(NEWID(),'LabDeProgramacion','7');
+
+INSERT INTO Materias(Id,NombreDeLaMateria,GradoDeLaMateria)
+Values(NEWID(),'Analysis y administracion de riesgo','9');
+
+
+
+INSERT INTO Nomina(NumeroDeEmpleado,Horas,Pago)
+Values(NEWID(),15,12);
+
+INSERT INTO Nomina(NumeroDeEmpleado,Horas,Pago)
+Values(NEWID(),15,11);
+
+INSERT INTO Nomina(NumeroDeEmpleado,Horas,Pago)
+Values(NEWID(),4,3);
+
+INSERT INTO Nomina(NumeroDeEmpleado,Horas,Pago)
+Values(NEWID(),6,8);
+
+INSERT INTO Nomina(NumeroDeEmpleado,Horas,Pago)
+Values(NEWID(),7,9);
+
+INSERT INTO Nomina(NumeroDeEmpleado,Horas,Pago)
+Values(NEWID(),15,7);
+
+INSERT INTO Nomina(NumeroDeEmpleado,Horas,Pago)
+Values(NEWID(),16,6);
+
+INSERT INTO Nomina(NumeroDeEmpleado,Horas,Pago)
+Values(NEWID(),66,2);
+
+INSERT INTO Nomina(NumeroDeEmpleado,Horas,Pago)
+Values(NEWID(),65,78);
+
+INSERT INTO Nomina(NumeroDeEmpleado,Horas,Pago)
+Values(NEWID(),32,2);
+
+
+
+INSERT INTO PagosDeColegiatura(Matricula,Inscripcion,Materias,Beca)
+Values(NEWID(),1,2,3);
+
+INSERT INTO PagosDeColegiatura(Matricula,Inscripcion,Materias,Beca)
+Values(NEWID(),3,2,5);
+
+INSERT INTO PagosDeColegiatura(Matricula,Inscripcion,Materias,Beca)
+Values(NEWID(),4,66,4);
+
+INSERT INTO PagosDeColegiatura(Matricula,Inscripcion,Materias,Beca)
+Values(NEWID(),5,4,8);
+
+INSERT INTO PagosDeColegiatura(Matricula,Inscripcion,Materias,Beca)
+Values(NEWID(),9,7,12);
+
+INSERT INTO PagosDeColegiatura(Matricula,Inscripcion,Materias,Beca)
+Values(NEWID(),55,4,7);
+
+INSERT INTO PagosDeColegiatura(Matricula,Inscripcion,Materias,Beca)
+Values(NEWID(),5,66,23);
+
+INSERT INTO PagosDeColegiatura(Matricula,Inscripcion,Materias,Beca)
+Values(NEWID(),2,45,7);
+
+INSERT INTO PagosDeColegiatura(Matricula,Inscripcion,Materias,Beca)
+Values(NEWID(),25,45,6);
+
+INSERT INTO PagosDeColegiatura(Matricula,Inscripcion,Materias,Beca)
+Values(NEWID(),5,44,4);
+
+
+INSERT INTO Salones(Id,Nombre,CantidadDeAsientos,Ubicacion)
+Values(NEWID(),'pri',2,'piso1');
+
+INSERT INTO Salones(Id,Nombre,CantidadDeAsientos,Ubicacion)
+Values(NEWID(),'segun',6,'piso1');
+
+INSERT INTO Salones(Id,Nombre,CantidadDeAsientos,Ubicacion)
+Values(NEWID(),'terce',6,'piso2');
+
+INSERT INTO Salones(Id,Nombre,CantidadDeAsientos,Ubicacion)
+Values(NEWID(),'cuarto',66,'piso2');
+
+INSERT INTO Salones(Id,Nombre,CantidadDeAsientos,Ubicacion)
+Values(NEWID(),'quint',45,'piso2');
+
+INSERT INTO Salones(Id,Nombre,CantidadDeAsientos,Ubicacion)
+Values(NEWID(),'sexto',12,'piso3');
+
+INSERT INTO Salones(Id,Nombre,CantidadDeAsientos,Ubicacion)
+Values(NEWID(),'septi',133,'piso4');
+
+INSERT INTO Salones(Id,Nombre,CantidadDeAsientos,Ubicacion)
+Values(NEWID(),'octo',45,'piso4');
+
+INSERT INTO Salones(Id,Nombre,CantidadDeAsientos,Ubicacion)
+Values(NEWID(),'nove',66,'piso5');
+
+INSERT INTO Salones(Id,Nombre,CantidadDeAsientos,Ubicacion)
+Values(NEWID(),'deci',44,'piso6');
+
+
+
+INSERT INTO TallaUniforme(Id,UniformeDeportivo,UniformeGala)
+Values(NEWID(),5,1);
+
+INSERT INTO TallaUniforme(Id,UniformeDeportivo,UniformeGala)
+Values(NEWID(),9,4);
+
+INSERT INTO TallaUniforme(Id,UniformeDeportivo,UniformeGala)
+Values(NEWID(),3,1);
+
+INSERT INTO TallaUniforme(Id,UniformeDeportivo,UniformeGala)
+Values(NEWID(),5,6);
+
+INSERT INTO TallaUniforme(Id,UniformeDeportivo,UniformeGala)
+Values(NEWID(),9,10);
+
+INSERT INTO TallaUniforme(Id,UniformeDeportivo,UniformeGala)
+Values(NEWID(),15,2);
+
+INSERT INTO TallaUniforme(Id,UniformeDeportivo,UniformeGala)
+Values(NEWID(),4,32);
+
+INSERT INTO TallaUniforme(Id,UniformeDeportivo,UniformeGala)
+Values(NEWID(),4,65);
+
+INSERT INTO TallaUniforme(Id,UniformeDeportivo,UniformeGala)
+Values(NEWID(),5,44);
+
+INSERT INTO TallaUniforme(Id,UniformeDeportivo,UniformeGala)
+Values(NEWID(),6,21);
+
+
+--10 inserts extras 
+
+
+INSERT INTO TallaUniforme(Id,UniformeDeportivo,UniformeGala)
+Values(NEWID(),140,130);
+
+INSERT INTO TallaUniforme(Id,UniformeDeportivo,UniformeGala)
+Values(NEWID(),445,445);
+
+INSERT INTO Salones(Id,Nombre,CantidadDeAsientos,Ubicacion)
+Values(NEWID(),'once',44,'piso8');
+
+INSERT INTO Salones(Id,Nombre,CantidadDeAsientos,Ubicacion)
+Values(NEWID(),'trece',44,'piso9');
+
+INSERT INTO Nomina(NumeroDeEmpleado,Horas,Pago)
+Values(NEWID(),25,244);
+
+INSERT INTO Nomina(NumeroDeEmpleado,Horas,Pago)
+Values(NEWID(),454,444);
+
+INSERT INTO Libros(Codigo,Edicion,FechaDePublicacion)
+Values(NEWID(),88,GETDATE());
+
+INSERT INTO Libros(Codigo,Edicion,FechaDePublicacion)
+Values(NEWID(),98,GETDATE());
+
+INSERT INTO Alumnos (Id,Nombre,ApellidoPaterno,ApellidoMaterno,Matricula)
+Values(NEWID(),'sapo','esponja','litle',1665486);
+
+INSERT INTO Alumnos (Id,Nombre,ApellidoPaterno,ApellidoMaterno,Matricula)
+Values(NEWID(),'angel','antonio','jersey',1665486);
+
+
+--Delete 
+
+DELETE from TallaUniforme where UniformeDeportivo = 140;
+DELETE from TallaUniforme where UniformeDeportivo = 445;
+DELETE from Salones where Nombre = 'once';
+DELETE from Salones where Nombre = 'trece';
+DELETE from Nomina where Horas = 244;
+DELETE from Nomina where Horas = 444;
+DELETE from Libros where Edicion = 88;
+DELETE from Libros where Edicion = 98;
+DELETE from Alumnos where Nombre = 'antonio';
+DELETE from Alumnos where Nombre = 'esponja';
+
+
+--Update
+
+Update TallaUniforme Set UniformeGala  = 20;
+Update TallaUniforme Set UniformeDeportivo=333 where UniformeGala < 30;
+Update Salones Set Nombre = 'nada';
+update Salones set Nombre = 'lleno';
+Update Nomina set Horas = 0;
+update Nomina set Horas = 10000 where Horas <30;
+update Libros set Edicion = 0;
+update Libros set Edicion= 322;
+update Alumnos set Nombre = 'Calamardo' where Nombre= 'Beto';
+update Alumnos set Nombre = 'Miguel Angel ' where Nombre ='amy';
+
+
+
